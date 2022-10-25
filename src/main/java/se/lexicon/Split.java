f1540ae859f06d1f22de6be6532ccc00f3813514
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-10-25 15:13:27 +0200

    Split_String

---
+++ src/main/java/se/lexicon/Split.java
@@ -0,0 +1,10 @@
+package se.lexicon;
+
+import java.util.Arrays;
+
+public class Split {
+        public static void main(String[] args) {
+            String originalString = "Carl,Susie,Fredrick,Bob,Erik" ;
+            String[] array = new String[] {originalString};
+            System.out.println(Arrays.toString(array));//printing the string array using Arrays.toString()
+        }  }
