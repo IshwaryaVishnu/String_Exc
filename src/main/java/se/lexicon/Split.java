6a49e8a6e23701740cc6cfde19a2dbc4332afae9
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-10-25 15:23:10 +0200

    Split_String

--- src/main/java/se/lexicon/Split.java
+++ src/main/java/se/lexicon/Split.java
@@ -1,10 +1,11 @@
 package se.lexicon;
-
-import java.util.Arrays;
-
 public class Split {
         public static void main(String[] args) {
-            String originalString = "Carl,Susie,Fredrick,Bob,Erik" ;
-            String[] array = new String[] {originalString};
-            System.out.println(Arrays.toString(array));//printing the string array using Arrays.toString()
-        }  }
+            String originalString = "Carl, Susie, Fredrick, Bob, Erik";
+            String[] arraySplit = originalString.split(", ");
+            for (String array : arraySplit)
+                System.out.println(array);
+        }
+}
+
+
