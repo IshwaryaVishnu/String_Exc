0970b55a4a88afda5b7f7237d9717e23e335fe0a
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-10-25 15:46:28 +0200

    Char_to_String

---
+++ src/main/java/se/lexicon/Char_to_String.java
@@ -0,0 +1,9 @@
+package se.lexicon;
+
+public class Char_to_String {
+    public static void main(String[] args) {
+        char[] charector ={'J','a','v','a'};
+    String string=String.valueOf(charector);
+System.out.println("String is: "+string);
+}
+}
