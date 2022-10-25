eb9b6c8a272807ae8db052fdc0f32696ca91e1f3
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-10-25 11:37:02 +0200

    Char_Index

--- src/main/java/se/lexicon/Char_Index.java
+++ src/main/java/se/lexicon/Char_Index.java
@@ -4,7 +4,6 @@ public class Char_Index {
     public static void main(String[] args)
     {
         String charIndex = "Long Example Sentence";  // string CharIndex
-        char index=charIndex.charAt(6);//returns the char value at the 6th index
-        System.out.println("The index position 6  is:" +(index));
+        System.out.println("The index position 6  is:" +charIndex.charAt(6)); //returns the char value at the 6th index
     }
 }
