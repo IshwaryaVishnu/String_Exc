1eef927abe9b88ff14b808ab76e309ffd76666df
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-10-25 11:14:06 +0200

    Substring

---
+++ src/main/java/se/lexicon/Sub_String.java
@@ -0,0 +1,9 @@
+package se.lexicon;
+
+public class Sub_String {
+        public static void main(String[] args)
+{
+    String originalString = "Ok this is not as long"; // string object
+    System.out.println("Sub String is:" + (originalString.substring(10,22)));
+}
+}
