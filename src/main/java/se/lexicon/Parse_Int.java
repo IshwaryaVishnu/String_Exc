e8cc8bd1bedd271e4439de415a1c0c44ff92fa2f
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-10-25 11:48:05 +0200

    Parse_Int

---
+++ src/main/java/se/lexicon/Parse_Int.java
@@ -0,0 +1,8 @@
+package se.lexicon;
+
+public class Parse_Int {
+    public static void main(String[] args) {
+        int original = 20 ;
+        System.out.println("ParseInt:" +original + Integer.parseInt("+20"));
+    }
+}
