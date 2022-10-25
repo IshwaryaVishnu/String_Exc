6becd094095f0a6bc0a4f723a4e993c3a58f7f54
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-10-25 15:24:12 +0200

    Split_String

--- src/main/java/se/lexicon/Split.java
+++ src/main/java/se/lexicon/Split.java
@@ -3,8 +3,7 @@ public class Split {
         public static void main(String[] args) {
             String originalString = "Carl, Susie, Fredrick, Bob, Erik";
             String[] arraySplit = originalString.split(", ");
-            for (String array : arraySplit)
-                System.out.println(array);
+            for (String array : arraySplit) System.out.println(array);
         }
 }
 
