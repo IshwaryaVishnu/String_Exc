6f5ddcd527ea0c081b18392fd6f5d8701a0ecc3a
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-10-25 11:50:52 +0200

    Integer_ParseInt

--- src/main/java/se/lexicon/Integer_ParseInt.java
+++ src/main/java/se/lexicon/Integer_ParseInt.java
@@ -1,6 +1,6 @@
 package se.lexicon;
 
-public class Parse_Int {
+public class Integer_ParseInt {
     public static void main(String[] args) {
         int original = 20 ;
         System.out.println("ParseInt:" +original + Integer.parseInt("+20"));
