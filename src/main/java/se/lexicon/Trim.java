63bfcfd3909b59d3f9450a0369fffe46dbe22f96
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-10-25 11:37:32 +0200

    Trim

---
+++ src/main/java/se/lexicon/Trim.java
@@ -0,0 +1,8 @@
+package se.lexicon;
+
+public class Trim {
+    public static void main(String[] args) {
+        String originalString = "\tJ\ta\tv\ta\t" ; // string object
+        System.out.println("Trim:" + (originalString.trim()));
+    }
+}
