a7f538ea8ba5aa280045aacc11738b9bf6ac354a
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-10-25 11:28:19 +0200

    Replace_String

---
+++ src/main/java/se/lexicon/Replace_String.java
@@ -0,0 +1,8 @@
+package se.lexicon;
+
+public class Replace_String {
+    public static void main(String[] args) {
+        String originalString = "Java is the worst programming language!"; // string object
+        System.out.println("Replace String:" + (originalString.replace("worst", "best")));
+    }
+}
